5e0c7856aba6a6a9707b322e3cd84701c5e7c364
anast <anast@LESHAMIR>
2023-09-13 01:40:29 +0300

    Добавьте файлы проекта.

---
+++ 06.09.2023 dz grafic/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace _06._09._2023_dz_grafic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(484, 461);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(460, 394);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 424);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Выберете функцию";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "y = kx",
+            "y = kx + b;",
+            "y = x^2",
+            "y = ax^2 + bx + c",
+            "y = x^3",
+            "y = x^1/2",
+            "y = sinx"});
+            this.comboBox1.Location = new System.Drawing.Point(193, 421);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(361, 412);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Построить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ 06.09.2023 dz grafic/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _06._09._2023_dz_grafic
+{
+    public partial class Form1 : Form
+    {
+        int indexCombobox;
+        Graphics graphics;
+        Bitmap bitmap;
+        Graphics graff; 
+        public Form1()
+        {
+            InitializeComponent();
+            bitmap = new Bitmap(this.Width, this.Height);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            graphics = Graphics.FromImage(pictureBox1.Image = bitmap);
+            graphics.DrawLine(Pens.Black, 0, pictureBox1.Height / 2, pictureBox1.Width, pictureBox1.Height / 2);
+            graphics.DrawLine(Pens.Black, pictureBox1.Width / 2, 0, pictureBox1.Width / 2, pictureBox1.Height);
+
+            graff = pictureBox1.CreateGraphics();
+            graff = Graphics.FromImage(pictureBox1.Image = bitmap);
+
+            int a = 1, b = 0, c = 0;
+            double x, y;
+            int _x = pictureBox1.Width / 2;
+            int _y = pictureBox1.Height / 2;
+
+            Bitmap bit = new Bitmap(1, 1);
+            bit.SetPixel(0, 0, Color.Blue);
+            switch (indexCombobox)
+            {
+                case 0:
+                    graff.DrawLine(Pens.Blue, 50, 378, 378, 50);
+                    break;
+                case 1:
+                    graff.DrawLine(Pens.Blue, 50, 358, 378, 30);
+                    break;
+                case 2:
+                    for (double i = 0; i < 100; i += 0.01)
+                    {
+                        x = i;
+                        y = (a * (x * x)) + ((b * x) + c);
+                        graff.DrawImageUnscaled(bit, _x + (int)x, _y - (int)y);
+                        graff.DrawImageUnscaled(bit, _x - (int)x, _y - (int)y);
+                    }
+                    break;
+                case 3:
+                    int _x2 = 280;
+                    int _y2 = 190;
+
+                    for (double i = 0; i < 10000; i += 0.01)
+                    {
+                        x = i;
+                        y = (a * (x * x)) + ((b * x) + c);
+                        graff.DrawImageUnscaled(bit, _x2 + (int)x, _y2 - (int)y);
+                        graff.DrawImageUnscaled(bit, _x2 - (int)x, _y2 - (int)y);
+                    }
+                    break;
+                case 4:
+                    for (double i = 0; i < 10000; i += 0.01)
+                    {
+                        x = i;
+                        y = (a * (x * x)) + ((b * x) + c);
+                        graff.DrawImageUnscaled(bit, _x + (int)x, _y - (int)y);
+                        graff.DrawImageUnscaled(bit, _x - (int)x, _y + (int)y);
+                    }
+                    break;
+                case 5:
+                    int _x3 = pictureBox1.Width / 2;
+                    int _y3 = pictureBox1.Height / 2;
+
+                    for (double i = 0; i < 10000; i += 0.01)
+                    {
+                        x = i;
+                        y = (a * (x * x)) + ((b * x) + c);
+                        graff.DrawImageUnscaled(bit, _x3 + (int)y, _y3 - (int)x);
+                    }
+                    break;
+
+                case 6:
+                    Point[] points = new Point[1000];
+
+                    for (int i = 0; i < points.Length; i++)
+                    {
+                        points[i] = new Point(i, (int)(Math.Sin((double)i / 10) * 10 + pictureBox1.Height / 2));
+                    }
+                    graff.DrawLines(Pens.Blue, points);
+                    break;
+            }
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch(comboBox1.SelectedIndex)
+            {
+                case 0:
+                    indexCombobox = 0; break;
+                case 1:
+                    indexCombobox = 1; break;
+                case 2:
+                    indexCombobox = 2; break;
+                case 3:
+                    indexCombobox = 3; break;
+                case 4:
+                    indexCombobox = 4; break;
+                case 5:
+                    indexCombobox = 5; break;
+                case 6:
+                    indexCombobox = 6; break;
+            }
+        }
+    }
+}
